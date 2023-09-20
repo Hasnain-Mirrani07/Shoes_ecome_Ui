0823371589b3164dba5bf71a5b74ce1bff32fcd8
Hasnain-Mirrani07 <115531664+Hasnain-Mirrani07@users.noreply.github.com>
2023-09-20 17:26:48 +0500

    test

--- nicke_shoping/lib/main.dart
+++ nicke_shoping/lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
 import 'package:nicke_shoping/pages/homepage.dart';
-//demo of startning ecome
 void main() =>
     runApp(MaterialApp(debugShowCheckedModeBanner: false, home: HomePage()));
